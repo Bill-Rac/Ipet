b793a086c248296e0ba561a123caf4b18c00f2bf
Billie E <hellbeliever@MacBook-Pro-de-Billie.local>
2023-06-13 19:14:12 -0500

    Tu madre tiene una polla, que ya la quisiera yo

--- client/src/components/Login/SignIn.jsx
+++ client/src/components/Login/SignIn.jsx
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { BsX } from "react-icons/bs";
 
 const SignIn = ({ onClose }) => {
+  const [activeTab, setActiveTab] = useState("register");
+
   const handleClose = () => {
     onClose();
+  };
+
+  const handleRegisterTab = () => {
+    setActiveTab("register");
+  };
+
+  const handleLoginTab = () => {
+    setActiveTab("login");
   };
 
   return (
     <LoginWrapper>
       <ContentWrapper>
-        <TitleWrapper>
-          <Title>¡Register!</Title>
-          <BsXIcon onClick={handleClose} />
-        </TitleWrapper>
-        <Input type="email" placeholder="E-mail Address" />
-        <Input type="password" placeholder="Password" />
-        <Button>LOGIN</Button>
+        <TabsWrapper>
+          <TabButton
+            active={activeTab === "register"}
+            onClick={handleRegisterTab}
+          >
+            Sign Up
+          </TabButton>
+          <TabButton active={activeTab === "login"} onClick={handleLoginTab}>
+            Sign In
+          </TabButton>
+        </TabsWrapper>
+        {activeTab === "register" ? (
+          <>
+            <TitleWrapper>
+              <Title>Register</Title>
+              <BsXIcon onClick={handleClose} />
+            </TitleWrapper>
+            <Input type="email" placeholder="E-mail Address" />
+            <Input type="password" placeholder="Password" />
+            <Button>Create account</Button>
+          </>
+        ) : (
+          <>
+            <TitleWrapper>
+              <Title>Login</Title>
+              <BsXIcon onClick={handleClose} />
+            </TitleWrapper>
+            <Input type="email" placeholder="E-mail Address" />
+            <Input type="password" placeholder="Password" />
+            <Button>Sign In</Button>
+          </>
+        )}
       </ContentWrapper>
     </LoginWrapper>
   );
@@ -44,6 +79,10 @@ const ContentWrapper = styled.div`
   padding: 20px;
   border-radius: 4px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+
+  @media (max-width: 480px) {
+    width: 90%;
+  }
 `;
 
 const TitleWrapper = styled.div`
@@ -62,6 +101,7 @@ const BsXIcon = styled(BsX)`
   right: 0;
   transform: translateY(-50%);
   cursor: pointer;
+  font-size: 25px;
 `;
 
 const Input = styled.input`
@@ -90,5 +130,28 @@ const Button = styled.button`
     background-color: white;
     border: 1px solid #603e85;
     color: #653f90;
+  }
+`;
+
+const TabsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+
+  @media (max-width: 480px) {
+    flex-wrap: wrap;
+  }
+`;
+
+const TabButton = styled.button`
+  padding: 8px 16px;
+  font-size: 16px;
+  border: none;
+  background-color: transparent;
+  color: ${(props) => (props.active ? "#653f90" : "black")};
+  cursor: pointer;
+
+  @media (max-width: 480px) {
+    flex-basis: 50%;
   }
 `;
