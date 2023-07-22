ab8acc87758968485d6d1092d3399ade2a850c60
Neilot <Jpx@gmail.com>
2023-07-21 19:42:57 -0500

    [B-01] Create a basic configuration for the backend server

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -8,7 +8,7 @@ const { SERVER_PORT } = process.env;
 
 app.get("/test", (_req, res) => {
   console.log("Someone is here!");
-  res.send("Hi!");
+  res.send("Hi!!!");
 });
 
 app.listen(SERVER_PORT, () => console.log(`Server on port ${SERVER_PORT}`));
