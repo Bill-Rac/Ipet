57c710e3ed75d92bc705d3e5e7d06cda335d7734
Neilot <Jpx165@gmail.com>
2023-07-21 20:04:35 -0500

    Backend initial setup (#1)
    * [B-01] Create a basic configuration for the backend server
    
    * [B-01] Removed the unused configurations from the TypeScript configuration file
    
    ---------
    
    Co-authored-by: Neilot <Jpx@gmail.com>

---
+++ Backend/src/server.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const { SERVER_PORT } = process.env;
+
+app.get("/test", (_req, res) => {
+  console.log("Someone is here!");
+  res.send("Hi!!!");
+});
+
+app.listen(SERVER_PORT, () => console.log(`Server on port ${SERVER_PORT}`));
