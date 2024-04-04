7256ecf8e59ed5efd9f6692a63f7cfa70bee64e0
B <hellbeliever@MacBook-Pro-de-B.local>
2024-04-03 19:15:25 -0500

    Cambios finales retoquesitos bonitirijillos

--- src/components/Admin/Layout.jsx
+++ src/components/Admin/Layout.jsx
@@ -1,21 +1,26 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import styled from "styled-components";
 import AdminNav from "./AdminNav";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Layout({ children }) {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, user } = useAuth0();
 
+  if (!isAuthenticated) {
+    return <Navigate replace to="/" />;
+  }
+
+  if (!user["dev-m1ynq7d6coq7obxp/roles"].includes("Admin")) {
+    return <Navigate replace to="/" />;
+  }
   return (
-    isAuthenticated && (
-      <LogginAnuncement>
-        <AdminNav />
-        <InformationText>{children}</InformationText>
-      </LogginAnuncement>
-    )
+    <LogginAnuncement>
+      <AdminNav />
+      <InformationText>{children}</InformationText>
+    </LogginAnuncement>
   );
 }
-
 const LogginAnuncement = styled.div`
   background-color: #8e2de2;
   min-height: 100vh;
